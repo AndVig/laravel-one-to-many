17c43fbe11c9841020961640e1216dc9b082a997
AndVig <andrea.vigna2@gmail.com>
2024-07-23 16:28:00 +0200

    [UPD] creato TypeSeeder e gestito migrazioni

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use illuminate\Support\Str;
 
 class TypeSeeder extends Seeder
 {
@@ -12,6 +14,16 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Type::truncate();
+
+        $types=['Frontend','Backend','Fullstack','Temp'];
+        foreach($types as $type){
+            $new_type=new Type();
+
+            $new_type->name=$type;
+            $new_type->slug=Str::of($type)->slug();
+
+            $new_type->save();
+        }
     }
 }
